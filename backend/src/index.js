afb84eb9a003a118398e0b2d4b13ade164b7d42b
Fatima <youremail@example.com>
2025-12-21 09:24:19 +0000

    new statis 8

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ const FormModel = require("./Model/FormModel");   // ✅ सही import
 const allowedOrigins = [
   // "https://amazone-shopping-front.onrender.com",
   "https://new-shop-g5i2.onrender.com",
-  "http://localhost:3000"
+  // "http://localhost:3000"
 ];
 
 app.use(cors({
